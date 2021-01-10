c0a9ea12b220503949c2142b87c0d122d61ebb2f
marcel <marcel@rutherford.ave>
2021-01-10 12:13:13 -0800

    name change

--- install-python-tf-dev-env.sh
+++ install-python-tf-dev-env.sh
@@ -19,7 +19,6 @@ source ~/python-envs/env/bin/activate
 sudo apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
 pip3 install -U pip testresources setuptools==49.6.0
 
-# tensorflow requirements
 pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 # version 2.x
 # sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v43 tensorflow-gpu
@@ -27,6 +26,7 @@ pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_prep
 pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 'tensorflow<2'
 pip3 install tensorflow-object-detection-api
 
+# some other things
 pip3 install wheel
 pip3 install pandas
 pip3 install matplotlib
